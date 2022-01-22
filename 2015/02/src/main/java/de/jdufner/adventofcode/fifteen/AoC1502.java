6697d1d3eef4dba93b73823a48681df96273faf1
Jürgen Dufner <juergen@jdufner.de>
2022-01-22 16:07:59 +0100

    2015-12-02, Part 1

--- 2015/02/src/main/java/de/jdufner/adventofcode/fifteen/AoC1502.java
+++ 2015/02/src/main/java/de/jdufner/adventofcode/fifteen/AoC1502.java
@@ -1,5 +1,42 @@
 package de.jdufner.adventofcode.fifteen;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
 class AoC1502 {
 
+    static int calculateSquareFeetOfWrappingPaper(int length, int width, int height) {
+        int lengthWidth = length * width;
+        int lengthHeight = length * height;
+        int widthHeight = width * height;
+        return lengthWidth * 2 + lengthHeight * 2 + widthHeight * 2 + smallest(lengthWidth, lengthHeight, widthHeight);
+    }
+
+    private static int smallest(int lengthWidth, int lengthHeight, int widthHeight) {
+        if (lengthWidth < lengthHeight && lengthWidth < widthHeight) {
+            return lengthWidth;
+        }
+        if (lengthHeight < widthHeight) {
+            return lengthHeight;
+        }
+        return widthHeight;
+    }
+
+    static String[] parseString(String s) {
+        return s.split("x");
+    }
+
+    static int calculateSquareFeetOfWrappingPaper(String s) {
+        String[] strings = parseString(s);
+        return calculateSquareFeetOfWrappingPaper(Integer.valueOf(strings[0]), Integer.valueOf(strings[1]), Integer.valueOf(strings[2]));
+    }
+
+    int calculateSumOfWrappingPaper(String filename) throws Exception {
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            return stream.mapToInt(AoC1502::calculateSquareFeetOfWrappingPaper).sum();
+        }
+        //return 0;
+    }
 }
--- 2015/02/src/test/java/de/jdufner/adventofcode/fifteen/AoC1502Test.java
+++ 2015/02/src/test/java/de/jdufner/adventofcode/fifteen/AoC1502Test.java
@@ -2,18 +2,78 @@ package de.jdufner.adventofcode.fifteen;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AoC1502Test {
 
     @Test
-    void part1() throws Exception {
+    void testExample1() {
         // arrange
 
         // act
+        int squareFeetOfWrappingPaper = AoC1502.calculateSquareFeetOfWrappingPaper(2, 3, 4);
 
         // assert
-        assertTrue(false);
+        assertEquals(58, squareFeetOfWrappingPaper);
+    }
+
+    @Test
+    void testExample2() {
+        // arrange
+
+        // act
+        int squareFeetOfWrappingPaper = AoC1502.calculateSquareFeetOfWrappingPaper(1, 1, 10);
+
+        // assert
+        assertEquals(43, squareFeetOfWrappingPaper);
+    }
+
+    @Test
+    void testExampleAsString1() {
+        // arrange
+
+        // act
+        int squareFeetOfWrappingPaper = AoC1502.calculateSquareFeetOfWrappingPaper("2x3x4");
+
+        // assert
+        assertEquals(58, squareFeetOfWrappingPaper);
+    }
+
+    @Test
+    void testExampleAsString2() {
+        // arrange
+
+        // act
+        int squareFeetOfWrappingPaper = AoC1502.calculateSquareFeetOfWrappingPaper("1x1x10");
+
+        // assert
+        assertEquals(43, squareFeetOfWrappingPaper);
+    }
+
+    @Test
+    void testParseString() {
+        // arrange
+
+        // act
+        String[] strings = AoC1502.parseString("1x2x3");
+
+        // assert
+        assertArrayEquals(new String[]{"1", "2", "3"}, strings);
+    }
+
+    @Test
+    void part1() throws Exception {
+        // arrange
+        AoC1502 aoC1502 = new AoC1502();
+
+        // act
+        int sumOfWrapperingPaper = aoC1502.calculateSumOfWrappingPaper("./src/test/resources/input");
+
+        // assert
+        assertEquals(1588178, sumOfWrapperingPaper);
     }
 
     @Test
