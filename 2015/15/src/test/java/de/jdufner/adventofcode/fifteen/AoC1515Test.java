b3714a8ae222df0a232f9d317cd15c330f1d9c80
Jürgen Dufner <juergen@jdufner.de>
2022-02-06 01:19:07 +0100

    2015-12-15, Preparation

---
+++ 2015/15/src/test/java/de/jdufner/adventofcode/fifteen/AoC1515Test.java
@@ -0,0 +1,25 @@
+package de.jdufner.adventofcode.fifteen;
+
+import org.junit.jupiter.api.Test;
+
+class AoC1515Test {
+
+    @Test
+    void part1() throws Exception {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void part2() throws Exception {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+}
